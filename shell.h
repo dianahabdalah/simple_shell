dea0f0a5fbfc74ac88262ee03c6950513d92fa19
dianahabdalah <dianahabdalah@gmail.com>
2023-09-20 17:17:14 -0700

    grp project task 0

--- shell.h
+++ shell.h
@@ -1,33 +1,128 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <signal.h>
-#include <string.h>
+#include <limits.h>
+#include <fcntl.h>
 #include <errno.h>
 
-/* Maximum length for user input and command */
-#define MAX_INPUT_LENGTH 1024
-#define MAX_ARG_LENGTH 64
+/* Constants for buffer sizes and command chaining types */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+#define CMD_NORM 0
+#define CMD_OR 1
+#define CMD_AND 2
+#define CMD_CHAIN 3
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+#define HIST_FILE ".simple_shell_history"
+#define HIST_MAX 4096
 
-/* Global environment variables */
-extern char **environ;
+/* Struct for singly linked list of strings */
+typedef struct liststr
+{
+    int num;
+    char *str;
+    struct liststr *next;
+} list_t;
+
+/* Struct for passing information between functions */
+typedef struct passinfo
+{
+    char *arg;
+    char **argv;
+    char *path;
+    int argc;
+    unsigned int line_count;
+    int err_num;
+    int linecount_flag;
+    char *fname;
+    list_t *env;
+    list_t *history;
+    list_t *alias;
+    char **environ;
+    int env_changed;
+    int status;
+    char **cmd_buf;
+    int cmd_buf_type;
+    int readfd;
+    int histcount;
+} info_t;
 
 /* Function prototypes */
-void display_prompt(void);
-char *read_input(void);
-char **parse_input(char *input);
-int execute_command(char **args);
-char *_strdup(const char *str);
-char *_strcat(char *dest, const char *src);
-char *_strchr(const char *s, int c);
-void _perror(char *prog_name, char *msg);
-void free_args(char **args);
+int is_delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
+char *convert_number(long int, int, int);
+void remove_comments(char *);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
+int bfree(void **);
+int interactive(info_t *);
+int print_d(int, int);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
+int is_chain(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+int replace_vars(info_t *);
+int replace_string(char **, char *);
+int hsh(info_t *, char **);
+int find_builtin(info_t *);
+void find_cmd(info_t *);
+void fork_cmd(info_t *);
+int is_cmd(info_t *, char *);
+char *dup_chars(char *, int, int);
+char *find_path(info_t *, char *, char *);
+void _eputs(char *);
+int _eputchar(char);
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+int _myhistory(info_t *);
+int _myalias(info_t *);
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void sigintHandler(int);
 
-#endif /* SHELL_H */
+#endif
 
