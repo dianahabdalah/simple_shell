7b2baf7bc67ad7b81508aea4a6e364d8d64f11db
dianahabdalah <dianahabdalah@gmail.com>
2023-09-20 17:07:34 -0700

    grp project task 0

--- shell.c
+++ shell.c
@@ -1,36 +1,13 @@
-#include "shell.h"
+#include <stdio.h>
 
 /**
- * main - Entry point of the shell program
+ * main - Entry point of the program
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char *input;
-char **args;
-int status;
-while (1)
-{
-/* Display the prompt */
-display_prompt();
-/* Read user input */
-input = read_input();
-
-/* Parse user input into arguments */
-args = parse_input(input);
-
-/* Execute the command */
-status = execute_command(args);
-
-/* Free allocated memory */
-free(input);
-free_args(args);
-
-/* Exit the shell on certain conditions */
-if (status == -1)
-break;
-}
-return (0);
+    printf("Hello, Betty!\n");
+    return (0);
 }
 
